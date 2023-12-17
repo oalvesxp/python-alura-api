0cb861c17e8aa32e5cfed34b8dc7706a7083f163
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-17 17:26:34 -0300

    updating function to add item in menu

--- app_delivery_v3/main.py
+++ app_delivery_v3/main.py
@@ -7,6 +7,9 @@ pizza = Eatery('Pizza Hut', 'Pizzaria')
 juice = Drink('Água com gás', 2.50, '350 ml')
 plate = Dish('Broto de Peperoni', 3.50, 'Pizza pequena de 4 pedaços')
 
+pizza.add_menu(juice)
+pizza.add_menu(plate)
+
 def main():
     print(juice)
     print(plate)
