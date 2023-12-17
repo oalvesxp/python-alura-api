60ab60bfbe57da1d80c1feaf96c5306847850400
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-17 17:13:28 -0300

    making tests

--- app_delivery_v3/main.py
+++ app_delivery_v3/main.py
@@ -1,22 +1,15 @@
 from models.restaurant import Eatery
+from models.menu.drink import Drink
+from models.menu.dish import Dish
 
-mexican = Eatery('Mexicaníssimo', 'Mexicano')
 pizza = Eatery('Pizza Hut', 'Pizzaria')
-japanese = Eatery('Nombei Izakaya', 'Japonesa')
 
-japanese.update_status()
-japanese.get_rating('Gui', 10)
-japanese.get_rating('Fabio', 7.5)
-japanese.get_rating('Nica', 7)
-japanese.get_rating('Junior', 5.5)
-japanese.get_rating('Junior', 3.5)
-
-pizza.update_status()
-pizza.get_rating('Joao', 8)
-pizza.get_rating('Maria', 5)
+juice = Drink('Água com gás', 2.50, '350 ml')
+plate = Dish('Broto de Peperoni', 3.50, 'Pizza pequena de 4 pedaços')
 
 def main():
-    Eatery.list_eatery()
+    print(juice)
+    print(plate)
 
 if __name__ == '__main__':
     main()
