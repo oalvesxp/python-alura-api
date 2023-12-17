11b0b8588d437044c4f11d7749d34d4a1fc841c3
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-17 17:55:26 -0300

    making tests

--- app_delivery_v3/main.py
+++ app_delivery_v3/main.py
@@ -5,7 +5,10 @@ from models.menu.dish import Dish
 pizza = Eatery('Pizza Hut', 'Pizzaria')
 
 juice = Drink('Água com gás', 2.50, '350 ml')
+juice.add_discount()
+
 plate = Dish('Broto de Peperoni', 3.50, 'Pizza pequena de 4 pedaços')
+plate.add_discount()
 
 pizza.add_menu(juice)
 pizza.add_menu(plate)
