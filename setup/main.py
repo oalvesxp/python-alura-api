2992f36eca1e4895fc71ca8146b847a196e02fc1
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-17 10:52:27 -0300

    Importing model and creating objects for validation

--- setup/main.py
+++ setup/main.py
@@ -1,5 +1,13 @@
+from models.restaurant import Eatery
+
+mexican = Eatery('Mexicaníssimo', 'Mexicano')
+pizza = Eatery('Pizza Hut', 'Pizzaria')
+japanese = Eatery('Nombei Izakaya', 'Japonesa')
+
+japanese.update_status()
+
 def main():
-    pass
+    Eatery.list_eatery()
 
 if __name__ == '__main__':
     main()
