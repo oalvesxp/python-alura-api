de80846cc6b2583acb17911134d4f749733751f5
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-17 10:08:45 -0300

    adding function for list restaurants

--- setup/models/restaurant.py
+++ setup/models/restaurant.py
@@ -1,6 +1,8 @@
-class Restaurant:
+class Eatery:
+    base = []
+
     def __init__(self, name, category):
-        '''Cria um objeto Restaurante.
+        '''Cria um objeto Restaurante e o adiciona à lista base.
         
         Input:
         - Nome
@@ -9,10 +11,24 @@ class Restaurant:
         self.name = name
         self.category = category
         self.status = False
+        Eatery.base.append(self)
 
     def __str__(self):
         return self.name
+    
+    def list_eatery():
+        '''Lista todos os restaurantes cadastrados
+        
+        Output:
+        - Nome
+        - Categoria
+        - Status
+        '''
+        for this in Eatery.base:
+            print(f'{this.name} | {this.category} | {this.status}')
 
 
-box_mineito = Restaurant('Box Mineiro', 'Caseiro')
-print(box_mineito)
+box_mineito = Eatery('Box Mineiro', 'Caseiro')
+box_mineito = Eatery('La Carbonara', 'Italiano')
+
+Eatery.list_eatery()
