72ad3c5524c098bd48995e82d52c81a1b32cf2f5
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-17 11:38:06 -0300

    making tests

--- setup/main.py
+++ setup/main.py
@@ -5,6 +5,15 @@ pizza = Eatery('Pizza Hut', 'Pizzaria')
 japanese = Eatery('Nombei Izakaya', 'Japonesa')
 
 japanese.update_status()
+japanese.get_rating('Gui', 10)
+japanese.get_rating('Fabio', 7.5)
+japanese.get_rating('Nica', 7)
+japanese.get_rating('Junior', 5.5)
+japanese.get_rating('Junior', 3.5)
+
+pizza.update_status()
+pizza.get_rating('Joao', 8)
+pizza.get_rating('Maria', 5)
 
 def main():
     Eatery.list_eatery()
